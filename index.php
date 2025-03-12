7d0eada6100d9e0729972edaecaf607332b155a8
vanhuy270623 <151001754+vanhuy270623@users.noreply.github.com>
2025-03-12 14:59:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ $airportData = json_decode($jsonData, true);
                         <h4>Tại Sao Nên Bay Cùng Chúng Tôi?</h4>
                         <ul class="social-links">
                             <li><a href=""><em>Cam kết giá tốt nhất</em></a></li>
-                            <li><a href="#"><em>Hơn 150 điểm đến trên toàn thế giới</em></a></li>
                             <li><a href="#"><em>Dịch vụ khách hàng cao cấp</em></a></li>
                             <li><a href="#"><em>Đặt chỗ an toàn & linh hoạt</em></a></li>
                         </ul>
